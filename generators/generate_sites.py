7eceafc11d58beda155cb71f6768985291251459
purajit <malalur2@illinois.edu>
2022-06-12 12:57:59 -0400

    simplify new generator, delete old generator

---
+++ generators/generate_sites.py
@@ -0,0 +1,111 @@
+import json
+import logging
+import os
+import sys
+
+from jinja2 import FileSystemLoader, Environment
+
+logging.basicConfig(stream=sys.stdout, level=logging.INFO)
+
+LOG = logging.getLogger()
+SITE_DIR = "./docs"
+DATA_DIR = "./data"
+TEMPLATES_DIR = "./templates"
+TEMPLATE_ENV = Environment(loader=FileSystemLoader(searchpath=TEMPLATES_DIR))
+
+
+def read_json_file(filename):
+    with open(os.path.join(DATA_DIR, filename)) as f:
+        return json.loads(f.read())
+
+
+def render_and_write(template_file, params, output_file):
+    output = TEMPLATE_ENV.get_template(template_file).render(**params)
+
+    with open(os.path.join(SITE_DIR, output_file), "w") as f:
+        f.write(output)
+
+
+def generate_page_params(level_data):
+    params = {}
+    if isinstance(level_data, dict):
+        for data_var, data_file in level_data.items():
+            params[data_var] = read_json_file(data_file)
+    elif isinstance(level_data, str):
+        params["data"] = level_data
+    else:
+        LOG.error("Level data must be dict or str")
+        raise Exception("invalid data type")
+    return params
+
+
+def generate_children_from_definition(children_data, level_name, next_level_path):
+    contents = []
+    for child_data in children_data["contents"]:
+        with open(os.path.join(DATA_DIR, level_name, child_data["route"])) as f:
+            data = f.read()
+        next_level = {
+            "template": children_data["template"],
+            "title": child_data["title"],
+            "route": child_data["route"],
+            "data": data,
+        }
+        contents.append(generate_level(next_level, next_level_path))
+    return contents
+
+
+def generate_children(children_data, level_path, level_name):
+    params = {}
+    if isinstance(children_data, list):
+        # children explicitly defined
+        params["contents"] = [
+            generate_level(child, level_path) for child in children_data]
+    elif isinstance(children_data, str):
+        # children to be generated from json definition
+        params["contents"] = generate_children_from_definition(
+            read_json_file(children_data), level_name, level_path)
+    else:
+        LOG.error("Children data must be list or str")
+        raise Exception("invalid children type")
+    return params
+
+
+def generate_level(level_map, previous_level_path):
+    level_name = level_map["route"]
+    level_path = os.path.join(previous_level_path, level_name)
+
+    if "children" in level_map:
+        output_file = os.path.join(level_path, 'index.html')
+        link = os.path.join("/", level_path)
+        children_data = level_map["children"]
+        os.makedirs(os.path.join(SITE_DIR, level_name), exist_ok=True)
+        additional_params = generate_children(children_data, level_path, level_name)
+    else:
+        # single page
+        link = os.path.join("/", level_path) + ".html"
+        output_file = level_path + ".html"
+        additional_params = generate_page_params(level_map.get("data", {}))
+
+    LOG.info(f"{previous_level_path} | {level_path} | {output_file} | {link}")
+
+    params = {
+        "title": level_map["title"],
+        "tab_title": level_map.get("tab_title"),
+        "previous_page": os.path.join('/', previous_level_path),
+        **additional_params
+    }
+    render_and_write(level_map["template"], params, output_file)
+
+    return {
+        "title": level_map["title"],
+        "link": link,
+    }
+
+
+def main():
+    sitemap = read_json_file("sitemap.json")
+    generate_level(sitemap, previous_level_path="")
+
+
+if __name__ == "__main__":
+    main()
