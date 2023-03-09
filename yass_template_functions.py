4f11adb52f8b5fc11b960b82be8e1c3464cadf2e
purajit <malalur2@illinois.edu>
2023-03-09 18:08:35 -0500

    use YASS (#52)

---
+++ yass_template_functions.py
@@ -0,0 +1,10 @@
+def yass_romanize(arabic):
+    conversions = [[1000, 'M'], [900, 'CM'], [500, 'D'], [400, 'CD'],
+                   [ 100, 'C'], [ 90, 'XC'], [ 50, 'L'], [ 40, 'XL'],
+                   [  10, 'X'], [  9, 'IX'], [  5, 'V'], [  4, 'IV'],
+                   [   1, 'I']]
+    result = ''
+    for denomination, roman_numeral in conversions:
+        result += roman_numeral * (arabic // denomination)
+        arabic %= denomination
+    return result
